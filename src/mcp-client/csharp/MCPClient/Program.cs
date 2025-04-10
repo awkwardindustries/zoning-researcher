33235439e13f0904a4b92fed4840728a4e1d1342
Megan Meyer <memeyer@microsoft.com>
2025-04-09 21:44:26 -0500

    Cleaned up creation to use function calling on the chat client. Updated to use SSE over stdio transport. Fixed typo on server.

--- src/mcp-client/csharp/MCPClient/Program.cs
+++ src/mcp-client/csharp/MCPClient/Program.cs
@@ -4,67 +4,33 @@ using MCPClient;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using ModelContextProtocol;
 using ModelContextProtocol.Client;
 using ModelContextProtocol.Protocol.Transport;
 using System.Text.Json;
 
 Console.WriteLine("===== Starting up the MCP Client =====");
 
-Console.WriteLine("Loading Environment Variables...");
+Console.WriteLine("\nLoading Environment Variables...");
 
 var root = Directory.GetCurrentDirectory();
-// We expect the shared .env to live at the project root, so
-// relative path from this "current directory":
-//   {project-root}/src/mcp-client/csharp/MCPClient
-var dotenv = Path.Combine(root, "../../../../.env");
+var dotenv = Path.Combine("../../../../.env");
 DotEnv.Load(dotenv);
 
-Console.WriteLine("Setting up logging...");
+Console.WriteLine("\nSetting up logging...");
 
 using var loggerFactory = LoggerFactory.Create(builder =>
     builder.AddConsole().SetMinimumLevel(LogLevel.Information));
 
-Console.WriteLine("Configuring MCP client options and server config...");
+Console.WriteLine("\nCreating the MCP Client...");
 
-var clientOptions = new McpClientOptions
-{
-    ClientInfo = new() { Name = "local-csharp-client", Version = "0.0.1" }
-};
-var serverConfig = new McpServerConfig
-{
-    Id = "fetch",
-    Name = "fetch",
-    TransportType = TransportTypes.StdIo,
-    TransportOptions = new Dictionary<string, string>
+IClientTransport clientTransport = new SseClientTransport(transportOptions: new()
     {
-        ["command"] = "uv",
-        ["arguments"] = @"run ../../../mcp-servers/browser/python/browser_tool"
-    }
-};
+        Endpoint = new Uri("http://localhost:8080/sse")
+    }, 
+    loggerFactory: loggerFactory);
+await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport, loggerFactory: loggerFactory);
 
-// var serverConfig = new McpServerConfig
-// {
-//     Id = "mcp-zoning-regulations-fetcher",
-//     Name = "mcp-zoning-regulations-fetcher",
-//     TransportType = TransportTypes.StdIo,
-//     TransportOptions = new Dictionary<string, string>
-//     {
-//         ["command"] = "uv",
-//         ["arguments"] = "run browser_tool"
-//     }
-// };
-
-// var clientTransport = new StdioClientTransport(new()
-// {
-//     Command = "uv",
-//     Arguments = "run browser_tool"
-// }, serverConfig, loggerFactory);
-
-Console.WriteLine("Creating the MCP Client...");
-
-await using var mcpClient =
-    await McpClientFactory.CreateAsync(serverConfig, clientOptions, loggerFactory: loggerFactory);
+Console.WriteLine("\nCreating the Chat Client...");
 
 var config = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
@@ -73,23 +39,26 @@ var config = new ConfigurationBuilder()
 string model = config["AZURE_OPENAI_MODEL_NAME"] ?? throw new ArgumentException("AZURE_OPENAI_MODEL_NAME is not set.");
 string key = config["AZURE_OPENAI_KEY"] ?? throw new ArgumentException("AZURE_OPENAI_KEY is not set.");
 string endpoint = config["AZURE_OPENAI_ENDPOINT"] ?? throw new ArgumentException("AZURE_OPENAI_ENDPOINT is not set.");
-var azureOpenAiClient = new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(key))
-    .AsChatClient(model);
 
-Console.WriteLine("Listing the MCP servers and tools...");
+using IChatClient chatClient = 
+    new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(key)).AsChatClient(model)
+    .AsBuilder()
+    .UseFunctionInvocation()
+    .Build();
+
+Console.WriteLine("\nListing the MCP servers and tools...");
 
 var mcpTools = await mcpClient.ListToolsAsync();
-var toolsJson = JsonSerializer.Serialize(mcpTools, new JsonSerializerOptions { WriteIndented = true });
+var toolsJson = JsonSerializer.Serialize(mcpTools, options: new(){ WriteIndented = true });
 Console.WriteLine("\nAvailable Tools:\n" + toolsJson);
 
 await Task.Delay(100);
 
-Console.WriteLine("===== MCP Client is ready! =====");
-Console.WriteLine("");
+Console.WriteLine("\n===== MCP Client is ready! =====\n");
 Console.WriteLine("Type your message below (or 'exit' to quit):");
 
-List<ChatMessage> chatHistory = new()
-{
+List<ChatMessage> chatHistory =
+[
     new(ChatRole.System, """
         You are a friendly assistant who helps people answer questions about building or adding
         to residences.
@@ -109,7 +78,7 @@ List<ChatMessage> chatHistory = new()
         If you don't know the answer or cannot find the location's zoning rules or regulations,
         just respond with that and see if there's anything else you might be able to help with.
     """),
-};
+];
 
 while(true)
 {
@@ -131,28 +100,16 @@ while(true)
 
     // Stream the AI response and add to chat history
 
-    // Console.WriteLine("Response <== ");
-    // var response = "";
-    // var chatOptions = new ChatOptions { Tools = [.. mcpTools] };
-    // await foreach (var item in azureOpenAiClient.GetStreamingResponseAsync(chatHistory, chatOptions))
-    // {
-    //     Console.Write(item.Text);
-    //     response += item.Text;
-    // }
-    // chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
-    // Console.WriteLine();
-
-    // Just gonna wait... blaming streaming for now.
-    try {
-        Console.WriteLine("Response <== ");
-        var chatOptions = new ChatOptions { Tools = [.. mcpTools], ToolMode = ChatToolMode.Auto };
-        var response = await azureOpenAiClient.GetResponseAsync(chatHistory, chatOptions);
-        var fromAssistant = response.Messages.LastOrDefault();
-        Console.WriteLine();
-        Console.WriteLine("DEBUG: " + response.Messages.Count + " Finish Reason: " + response.FinishReason + " Role: " + response.Messages[0].Role + " Text: " + response.Messages[0].Text);
-        chatHistory.Add(fromAssistant);
-        Console.WriteLine(fromAssistant.Text);
-    } catch (Exception e) {
-        Console.Error.WriteLine("UNEXPECTED ERROR: " + e.Message);
+    Console.WriteLine("Response <== ");
+    
+    List<ChatResponseUpdate> updates = [];
+    await foreach (var update in chatClient.GetStreamingResponseAsync(chatHistory, new() { Tools = [.. mcpTools]}))
+    {
+        Console.Write(update);
+        updates.Add(update);
     }
+    Console.WriteLine();
+
+    chatHistory.AddMessages(updates);
+
 }
